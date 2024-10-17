c499316e50e1a390d23169144c0960086a4d4328
Jas Ahluwalia <jas.ahluwalia@gmail.com>
2024-10-17 16:58:33 -0700

    decent place

--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,15 @@
 # Initialize Starship
 set -gx PATH /opt/homebrew/bin $PATH
 starship init fish | source
-
 # Add PATH entries
 set -x PATH $PATH /Users/jas/Library/Python/3.8/bin
 set -x PATH /Applications/GoLand.app/Contents/MacOS '/Applications/IntelliJ IDEA.app/Contents/MacOS' $PATH
-
-# Aliases
-alias zshconfig="vim ~/.config/fish/config.fish"
-alias ohmyzsh="vim ~/.oh-my-zsh"
 
 # Initialize Pyenv
 status --is-interactive; and pyenv init --path | source
 
 # Use lts for nvm
-nvm use lts
+#nvm use lts
 
 # Set AWS profile
 set -x AWS_PROFILE platform-dev
@@ -30,6 +25,9 @@ function sshp
     sshpass -p dragos ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -l dragos $argv
 end
 
+#Aliases
+thefuck  --alias | source
+ 
 # Goenv setup
 set -x GOENV_ROOT $HOME/.goenv
 set -x PATH $GOENV_ROOT/bin $PATH
--- test.sh
+++
@@ -1,7 +0,0 @@
-mkdir recovered_blobs  # Create a directory to store recovered files
-
-# Loop through each dangling blob
-for blob in $(git fsck --lost-found | grep "dangling blob" | awk '{print $3}'); do
-    # Create a file using the blob's hash and restore the contents
-    git show $blob > recovered_blobs/$blob.txt
-done
